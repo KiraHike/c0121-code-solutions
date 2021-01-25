674fce974f8afc45758172afa9c05cbf08fe0c02
Kira Hike <kirahikes@gmail.com>
2021-01-25 12:45:45 -0800

    completed js operators and expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,47 @@
+var width = 20;
+var height = 10;
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('typof area:', typeof area);
+
+var bill = 100;
+var payment = 125;
+var change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 90;
+var midterm = 83;
+var final = 91;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Kira';
+var lastName = 'Hike';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 8;
+var isAcidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 300;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta', isSparta);
+console.log('typeof isSparta', typeof isSparta);
+
+var motto = fullName;
+
+motto = motto + ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
