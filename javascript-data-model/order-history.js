21a47bbf1c3204902e6a206e07a0c9fb207851c9
Kira Hike <kirahikes@gmail.com>
2021-01-26 12:26:54 -0800

    added itemCategory property

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,7 +1,7 @@
 var orderHistory = [
   {
     orderDatePlaced: 'August 4, 2020',
-    orderNumber: 114 - 3941689 - 8772232,
+    orderNumber: '114-3941689-8772232',
     orderTotal: 34.00,
     orderShippedTo: 'JS Masher',
     orderDateDelivered: 'Aug 8, 2020',
@@ -9,6 +9,7 @@ var orderHistory = [
     orderItemList: [
       {
         itemImage: 'image',
+        itemCategory: 'Books',
         itemName: 'JavaScript for impatient programmers',
         itemAuthorName: 'Rauschmayer, Dr. Axel',
         itemTotal: 31.55,
@@ -19,7 +20,7 @@ var orderHistory = [
 
   {
     orderDatePlaced: 'July 19, 2020',
-    orderNumber: 113 - 9984268 - 1280257,
+    orderNumber: '113-9984268-1280257',
     orderTotal: 44.53,
     orderShippedTo: 'JS Masher',
     orderDateDelivered: 'Jul 20, 2020',
@@ -29,6 +30,7 @@ var orderHistory = [
       {
         itemImage: 'image',
         itemName: 'The Timeless Way of Building',
+        itemCategory: 'Books',
         itemAuthorName: 'Alexander, Christopher',
         itemTotal: 41.33,
         itemReturnCloseDate: 'Return window closed on Aug 19, 2020'
@@ -38,7 +40,7 @@ var orderHistory = [
 
   {
     orderDatePlaced: 'July 4, 2020',
-    orderNumber: 114 - 2875557 - 9059409,
+    orderNumber: '114-2875557-9059409',
     orderTotal: 17.22,
     orderShippedTo: 'JS Masher',
     orderDateDelivered: 'Jul 7, 2020',
@@ -47,9 +49,10 @@ var orderHistory = [
     orderItemList: [
       {
         itemImage: 'image',
+        itemCategory: 'Electronics',
         itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch ' +
-                'Gamecube Adapter for WII U, PC. Support Turbo and ' +
-                'Vibration Features. No Driver and No Lag-Gamecube Adapter',
+                  'Gamecube Adapter for WII U, PC. Support Turbo and ' +
+                  'Vibration Features. No Driver and No Lag-Gamecube Adapter',
         itemTotal: 15.98,
         itemReturnCloseDate: 'Return window closed on Aug 5, 2020'
       }
@@ -58,7 +61,7 @@ var orderHistory = [
 
   {
     orderDatePlaced: 'July 3, 2020',
-    orderNumber: 113 - 2883177 - 2648248,
+    orderNumber: '113-2883177-2648248',
     orderTotal: 138.93,
     orderShippedTo: 'JS Masher',
     orderDateDelivered: 'Jul 5, 2020',
@@ -66,6 +69,7 @@ var orderHistory = [
     orderItemList: [
       {
         itemImage: 'image',
+        itemCategory: 'Eletronics',
         itemName: 'GameCube Controller - Super Smash Bros. Edition ' +
                 '(Nintendo Switch)',
         itemTotal: 94.95,
@@ -74,6 +78,7 @@ var orderHistory = [
 
       {
         itemImage: 'image',
+        itemCategory: 'Books',
         itemName: 'The Art of Sql',
         itemAuthorName: 'Faroult, Stephane',
         itemTotal: 33.99,
