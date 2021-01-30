c18917f98b814477fb7030fe93229e6a2fba0153
Kira Hike <kirahikes@gmail.com>
2021-01-30 13:02:39 -0800

    completed array manipulation 2 | Kira Hike

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,11 @@
 /* exported chunk */
+
+function chunk(array, size) {
+  var chunkResult = [];
+  var indexVar = 0;
+  while (indexVar < array.length) {
+    chunkResult.push(array.slice(indexVar, size + indexVar));
+    indexVar = indexVar + size;
+  }
+  return chunkResult;
+}
