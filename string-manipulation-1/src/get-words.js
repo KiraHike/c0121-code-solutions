617e0941ed5205ed688cacf989dbf42d9fbe72f3
Kira Hike <kirahikes@gmail.com>
2021-01-29 16:59:47 -0800

    completed string manipulation 1 | Kira Hike

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,1 +1,11 @@
 /* exported getWords */
+
+function getWords(string) {
+  var getWordsEmpty = [];
+  var getWordsResult = string.split(' ');
+  if (string === '') {
+    return getWordsEmpty;
+  } else {
+    return getWordsResult;
+  }
+}
--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,1 +1,9 @@
 /* exported isVowel */
+
+function isVowel(char) {
+  var low = char.toLowerCase();
+  if (low === 'a' || low === 'e' || low === 'i' || low === 'o' || low === 'u') {
+    return true;
+  }
+  return false;
+}
