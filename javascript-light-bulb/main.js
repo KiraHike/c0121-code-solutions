257b707da3319c6b5211d09b6c4d19c9e43414d3
Kira Hike <kirahikes@gmail.com>
2021-02-02 11:57:55 -0800

    changed switch value to boolean | Kira Hike

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,16 +1,16 @@
 var $background = document.querySelector('.background');
 var $bulb = document.querySelector('.light-bulb');
-var lightSwitch = 'on';
+var lightSwitch = true;
 
 function lightBulb(event) {
-  if (lightSwitch === 'on') {
+  if (lightSwitch === true) {
     $background.className = 'background off';
     $bulb.className = 'light-bulb off';
-    lightSwitch = 'off';
+    lightSwitch = false;
   } else {
     $background.className = 'background on';
     $bulb.className = 'light-bulb on';
-    lightSwitch = 'on';
+    lightSwitch = true;
   }
 }
 
