05d982c1fae54e150ce7ca102eef6e79e730a2cf
Kira Hike <kirahikes@gmail.com>
2021-02-03 09:41:50 -0800

    completed js typing tutor | Kira Hike

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,24 +1,41 @@
 var $char = document.querySelectorAll('span');
-// var $modal = document.querySelector('modal');
+var $modal = document.querySelector('.modal');
+var $stats = document.querySelector('.stats');
+var $buttonYes = document.querySelector('.button-yes');
+var $buttonNo = document.querySelector('.button-no');
 var i = 0;
-// var correct = 0;
-// var incorrect = 0;
+var correct = 0;
+var incorrect = 0;
+var totalType = 0;
+var accuracy = 0;
 
 function type(event) {
-  // $modal = '.modal hidden';
   if ($char[i].textContent !== event.key) {
-    $char[i].className = '.span rd ul';
-    // incorrect++;
+    $char[i].className = 'span rd ul';
+    incorrect++;
   } else if (i + 1 < $char.length) {
-    $char[i].className = '.span gr';
-    $char[i + 1].className = '.span bl ul';
+    $char[i].className = 'span gr';
+    $char[i + 1].className = 'span bl ul';
     i++;
-    // correct++;
+    correct++;
   } else {
-    $char[i].className = '.span gr';
-    // correct++;
-    // $modal = '.modal visable';
+    $char[i].className = 'span gr';
+    correct++;
+    totalType = correct + incorrect;
+    accuracy = Math.floor(correct / totalType * 100);
+    $modal.className = 'modal visable';
+    $stats.textContent = 'Accuracy: ' + accuracy + '%';
   }
 }
 
+function no(event) {
+  $modal.className = 'modal hidden';
+}
+
+function yes(event) {
+  location.reload();
+}
+
 document.addEventListener('keydown', type);
+$buttonNo.addEventListener('click', no);
+$buttonYes.addEventListener('click', yes);
