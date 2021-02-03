b27d17f252c833996af03ef387eba6c94777e7e1
Kira Hike <kirahikes@gmail.com>
2021-02-02 23:30:00 -0800

    js typing tutor in progress 2 | Kira Hike

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,15 +1,24 @@
-// var $char = document.querySelectorAll('span');
-// console.log($char);
+var $char = document.querySelectorAll('span');
+// var $modal = document.querySelector('modal');
+var i = 0;
+// var correct = 0;
+// var incorrect = 0;
 
-// function type(event) {
-//   for (var i = 0; i < $char.length; i++) {
-//     if ($char[0].textContent !== event.key) {
-//       $char[0].className = '.span rd ul';
-//     } else {
-//       $char[0].className = '.span gr';
-//       $char[0 + 1].className = '.span bl ul';
-//     }
-//   }
-// }
+function type(event) {
+  // $modal = '.modal hidden';
+  if ($char[i].textContent !== event.key) {
+    $char[i].className = '.span rd ul';
+    // incorrect++;
+  } else if (i + 1 < $char.length) {
+    $char[i].className = '.span gr';
+    $char[i + 1].className = '.span bl ul';
+    i++;
+    // correct++;
+  } else {
+    $char[i].className = '.span gr';
+    // correct++;
+    // $modal = '.modal visable';
+  }
+}
 
-// document.addEventListener('keydown', type);
+document.addEventListener('keydown', type);
