39e1408c3228c4076eec2d6637675c0ea72489a2
Kira Hike <kirahikes@gmail.com>
2021-02-02 18:46:08 -0800

    js typing tutor in progress | Kira Hike

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,15 @@
+// var $char = document.querySelectorAll('span');
+// console.log($char);
+
+// function type(event) {
+//   for (var i = 0; i < $char.length; i++) {
+//     if ($char[0].textContent !== event.key) {
+//       $char[0].className = '.span rd ul';
+//     } else {
+//       $char[0].className = '.span gr';
+//       $char[0 + 1].className = '.span bl ul';
+//     }
+//   }
+// }
+
+// document.addEventListener('keydown', type);
