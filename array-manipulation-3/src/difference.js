f7b76dd0919713742cefd9966f0fa4f20e4544b5
Kira Hike <kirahikes@gmail.com>
2021-02-13 18:05:24 -0800

    completed array manipulation 3 | Kira Hike

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,18 @@
 /* exported difference */
+
+function difference(first, second) {
+  var differenceResult = [];
+  for (var i = 0; i < first.length; i++) {
+    var element = first[i];
+    if (second.indexOf(element) === -1) {
+      differenceResult.push(element);
+    }
+  }
+  for (i = 0; i < second.length; i++) {
+    element = second[i];
+    if (first.indexOf(element) === -1) {
+      differenceResult.push(element);
+    }
+  }
+  return differenceResult;
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,18 @@
 /* exported zip */
+
+function zip(first, second) {
+  var zipResult = [];
+  var lastIndex;
+  if (first.length <= second.length) {
+    lastIndex = first.length - 1;
+  } else {
+    lastIndex = second.length - 1;
+  }
+  for (var i = 0; i <= lastIndex; i++) {
+    var subArray = [];
+    subArray.push(first[i]);
+    subArray.push(second[i]);
+    zipResult.push(subArray);
+  }
+  return zipResult;
+}
