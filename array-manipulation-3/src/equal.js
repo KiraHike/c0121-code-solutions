3aabeb71b8fdfb994447907125b134b90f2bb775
Kira Hike <kirahikes@gmail.com>
2021-02-15 09:47:46 -0800

    revised equal to find longest array first | Kira Hike

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,12 +1,14 @@
 /* exported equal */
 
 function equal(first, second) {
-  var i = 0;
-  for (i; i < first.length; i++) {
-    for (i; i < second.length; i++) {
-      if (first[i] !== second[i]) {
-        return false;
-      }
+  if (first.length >= second.length) {
+    var longArray = first;
+  } else {
+    longArray = second;
+  }
+  for (var i = 0; i < longArray.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
     }
   }
   return true;
