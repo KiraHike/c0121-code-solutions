3ab3fba2f420334a8c033b176100a9690be0cbc9
KiraHike <kirahikes@gmail.com>
2021-03-11 15:00:50 -0800

    completed delete module

---
+++ node-notes-cli/delete.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+function deleteEntry(entryId) {
+  const file = require('./data.json');
+  for (const key in file.notes) {
+    if (key === entryId) {
+      delete file.notes[key];
+      const data = JSON.stringify(file, null, 2);
+      fs.writeFile('./data.json', data, err => {
+        if (err) throw err;
+      });
+    }
+  }
+}
+
+deleteEntry(process.argv[2]);
