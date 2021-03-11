03b8b613dc213074a141f3f37b501f0d684a0f54
KiraHike <kirahikes@gmail.com>
2021-03-11 15:09:44 -0800

    completed update module

---
+++ node-notes-cli/update.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+function updateEntry(entryId, updatedEntry) {
+  const file = require('./data.json');
+  for (const key in file.notes) {
+    if (key === entryId) {
+      file.notes[key] = updatedEntry;
+      const data = JSON.stringify(file, null, 2);
+      fs.writeFile('./data.json', data, err => {
+        if (err) throw err;
+      });
+    }
+  }
+}
+
+updateEntry(process.argv[2], process.argv[3]);
