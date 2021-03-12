c82817014172003817aeef2da26935ae52148671
KiraHike <kirahikes@gmail.com>
2021-03-11 16:56:43 -0800

    revised node notes cli update and delete functions

--- node-notes-cli/delete.js
+++ node-notes-cli/delete.js
@@ -2,15 +2,11 @@ const fs = require('fs');
 
 function deleteEntry(entryId) {
   const file = require('./data.json');
-  for (const key in file.notes) {
-    if (key === entryId) {
-      delete file.notes[key];
-      const data = JSON.stringify(file, null, 2);
-      fs.writeFile('./data.json', data, err => {
-        if (err) throw err;
-      });
-    }
-  }
+  delete file.notes[entryId];
+  const data = JSON.stringify(file, null, 2);
+  fs.writeFile('./data.json', data, err => {
+    if (err) throw err;
+  });
 }
 
 module.exports = deleteEntry;
--- node-notes-cli/update.js
+++ node-notes-cli/update.js
@@ -2,15 +2,11 @@ const fs = require('fs');
 
 function updateEntry(entryId, updatedEntry) {
   const file = require('./data.json');
-  for (const key in file.notes) {
-    if (key === entryId) {
-      file.notes[key] = updatedEntry;
-      const data = JSON.stringify(file, null, 2);
-      fs.writeFile('./data.json', data, err => {
-        if (err) throw err;
-      });
-    }
-  }
+  file.notes[entryId] = updatedEntry;
+  const data = JSON.stringify(file, null, 2);
+  fs.writeFile('./data.json', data, err => {
+    if (err) throw err;
+  });
 }
 
 module.exports = updateEntry;
