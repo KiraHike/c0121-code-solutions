8d4b2330f8b1393d30d2b7e661934e716311a3bc
KiraHike <kirahikes@gmail.com>
2021-03-11 17:12:37 -0800

    revised readAll function to include counter

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -4,11 +4,13 @@ const allFiles = process.argv;
 
 function readAll(files) {
   const completedArray = [];
+  let counter = 2;
   for (let i = 2; i < files.length; i++) {
     fs.readFile(files[i], 'utf8', (err, data) => {
       if (err) throw err;
       completedArray[i] = data;
-      if (files.length === completedArray.length) {
+      counter++;
+      if (files.length === counter) {
         for (i = 2; i < completedArray.length; i++) {
           console.log(completedArray[i]);
         }
