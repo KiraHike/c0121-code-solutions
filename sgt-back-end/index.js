1de26003d1922c67803d213a1f3a9222a70a1c92
KiraHike <kirahikes@gmail.com>
2021-03-20 17:06:00 -0700

    added index and get one module

---
+++ sgt-back-end/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const getGrade = require('./get');
+
+const app = express();
+const parseJSON = express.json();
+
+app.use(parseJSON);
+
+app.get('/api/grades/:gradeId', getGrade);
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express listening on port 3000');
+});
