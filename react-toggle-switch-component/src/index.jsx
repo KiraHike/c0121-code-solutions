7e404e5650236c6082a446acb13d2991ca72a4fd
KiraHike <kirahikes@gmail.com>
2021-03-25 16:59:21 -0700

    added react component

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ToggleSwitch from './toggle-switch';
+
+class App extends React.Component {
+  render() {
+    return <ToggleSwitch />;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
