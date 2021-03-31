142322a6852bd33a7728e802d3ff684ce45f829f
KiraHike <kirahikes@gmail.com>
2021-03-31 13:25:40 -0700

    changed to props input

--- react-accordion-component/src/accordian.jsx
+++ react-accordion-component/src/accordian.jsx
@@ -5,32 +5,6 @@ export default class Accordian extends React.Component {
     super(props);
     this.state = { view: 0 };
     this.handleClick = this.handleClick.bind(this);
-    this.topicsArray = [
-      {
-        id: 1,
-        name: 'Hypertext Markup Language',
-        about: `Hypertext Markup Language (HTML) is the standard markup language
-              for creating web pages and web applications. With Cascading Style
-              Sheets (CSS) and JavaScript, it forms a triad of cornerstone
-              technologies for the World Wide Web.`
-      },
-      {
-        id: 2,
-        name: 'Cascading Style Sheets',
-        about: `Cascading Style Sheets (CSS) is a style sheet language used for
-              describing the presentation of a document written in a markup
-              language like HTML. CSS is a cornerstone technology of the World
-              Wide Web alongside HTML and JavaScript.`
-      },
-      {
-        id: 3,
-        name: 'JavaScript',
-        about: `JavaScript, often abbreviated as JS, is a high-level, interpreted
-              programming language that conforms to the ECMAScript specification.
-              JavaScript has curly-bracket syntax, dynamic typing, prototype-based
-              object-orientation and first-class functions.`
-      }
-    ];
   }
 
   makeList(array) {
@@ -63,7 +37,7 @@ export default class Accordian extends React.Component {
   }
 
   render() {
-    const list = this.makeList(this.topicsArray);
+    const list = this.makeList(this.props.array);
     return (
       <>{list}</>
     );
